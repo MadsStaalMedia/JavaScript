9baabc5335ff77fafc6760c3ca64e182746464ad
Nina1665 <nina_foged@hotmail.dk>
2025-09-02 10:37:53 +0200

    omdøber navne

--- js/java.js
+++ js/java.js
@@ -1,27 +1,24 @@
+
 const msgs=[ // opretter en konstant variabel // msgs er en liste af objekter 
     {t:"Årslev Bibliotek",x:"Velkommen! Se arrangementer."},  // { t: "...", x: "..." } (array liste)
  {t:"Åbningstider",x:"Man-fre:10-17, Lør:10-13"}
 ];
-
-const o=document.createElement("div");  // laver en variabel o med et <div> element
-const p=document.createElement("div"); // laver endnu et <div>
-const b=document.createElement("button"); // laver et <button> element 
-
-o.style="position:fixed;inset:0;background:#f6f6f6;display:flex;align-items:center;justify-content:center";
-p.style="background:#FFF;padding:20px;border-radius:12px;text-align:center;font-family:sans-serif"; //CSS 
-
+const popupboks=document.createElement("div");  // laver en variabel popupboks med et <div> element
+const beholder=document.createElement("div"); // laver endnu et <div>
+const knap=document.createElement("button"); // laver et <button> element 
+popupboks.style="position:fixed;inset:0;background:#f6f6f6;display:flex;align-items:center;justify-content:center";
+beholder.style="background:#FFF;padding:20px;border-radius:12px;text-align:center;font-family:sans-serif"; //CSS 
 msgs.forEach(m=>{
     if(m&&m.t&&m.x){ // tjekker om objektet (m) har titel (t) og en tekst (x)
         const h=document.createElement("h2"); // laver en overskrift
-        const t =document.createElement("p"); // laver et afsnit <p> til pop'upen
+        const t =document.createElement("beholder"); // laver et afsnit <p> til pop'upen
         h.textContent=m.t; // sætter  tekstindholdet ind fra objektet 
         t.textContent=m.x;
-        p.append(h,t) // smider dem ind i pop-up boksen (p)
+        beholder.append(h,t) // smider dem ind i pop-up boksen (beholder)
     }else console.warn("Fejl")
 });
-
-b.textContent="Luk"; // giver knappen teksten ''luk'' 
-b.onclick=()=>o.remove(); // når man klikker, fjernes pop'upen (o)
-p.appendChild(b); //sætter knappen ind i boksen 
-o.appendChild(p); // lægger boksen ind i overlayet 
-document.body.appendChild(o); // sætter hele pop'upen ind i siden, så man kan se den 
+knap.textContent="Luk"; // giver knappen teksten ''luk'' 
+knap.onclick=()=>popupboks.remove(); // når man klikker, fjernes pop'upen (popupboks)
+beholder.appendChild(knap); //sætter knappen ind i boksen 
+popupboks.appendChild(beholder); // lægger boksen ind i overlayet 
+document.body.appendChild(popupboks); // sætter hele pop'upen ind i siden, så man kan se den  
