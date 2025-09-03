ede033efc88cd507a2a0dd33164956e10daf9d65
EENM25 <emilie.e.mat@gmail.com>
2025-09-03 10:39:31 +0200

    Billede ændring - hest

--- js/galleri.js
+++ js/galleri.js
@@ -1,4 +1,4 @@
-const indhold = ["et.jpeg", "to.jpg", "tre.png"];
+const indhold = ["baby1.jpg", "baby2.jpeg", "baby3.png"];
 
 let galleri = document.getElementById("Galleri");
 
@@ -10,12 +10,12 @@ for (let i = 0; i < indhold.length; i++) {
 function skiftbillede(x) {
 let stortbillede = 0;
 if (Number(x)==0) {
-    document.getElementById("billede").innerHTML='<img class="billedestor" src="et.jpeg"> '
+    document.getElementById("billede").innerHTML='<img class="billedestor" src="baby1.jpg"> '
 }
  if (Number(x)==1) {
-    document.getElementById("billede").innerHTML='<img class="billedestor" src="to.jpg"> '
+    document.getElementById("billede").innerHTML='<img class="billedestor" src="baby2.jpeg"> '
 }
 if (Number(x)==2) {
-    document.getElementById("billede").innerHTML='<img class="billedestor" src="tre.png"> '
+    document.getElementById("billede").innerHTML='<img class="billedestor" src="baby3.png"> '
 }
 }
