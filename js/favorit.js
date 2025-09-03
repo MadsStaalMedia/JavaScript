df7613997a96f8c85806be0225625898c3d64200
Jpha70545 <jpha70545@edu.ucl.dk>
2025-09-03 08:21:26 +0200

    CSS version

---
+++ js/favorit.js
@@ -0,0 +1,24 @@
+       
+       
+       let book = [
+            { navn: "Harry Potter",titel: "Flammernes pokal", forfatter: "J.K Rowiling"},
+            { navn: "Rolf og Ritter",titel: "Historier fra 20år", forfatter: "Dennis og Rolf"},
+            { navn: "Hækling",titel: "Strik og sjov hækling", forfatter: "Sophie Britten"},
+            { navn: "Krimi",titel:"Det forsvundne barn" , forfatter: "Linda Lassen"},
+            { navn: "Krimi",titel: "Havhesten", forfatter: "Vibeke Marx"}
+            { navn: "Ben Elton",titel: "I kamp mod tiden", forfatter: "Turbulenz"}
+        ];
+
+  
+
+        for(i=0; i < book.length; i++){
+            document.getElementById()
+        };
+
+
+
+
+        function farvoritKnap(){
+            alert("hello")
+        }
+
