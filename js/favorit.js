a3ebdb04920d8e7712549dd2c4125805ed630d2e
Jpha70545 <jpha70545@edu.ucl.dk>
2025-09-03 11:28:23 +0200

    array

--- js/favorit.js
+++ js/favorit.js
@@ -1,24 +1,47 @@
        
-       
+       console.log("tekst");
+
        let book = [
-            { navn: "Harry Potter",titel: "Flammernes pokal", forfatter: "J.K Rowiling"},
-            { navn: "Rolf og Ritter",titel: "Historier fra 20år", forfatter: "Dennis og Rolf"},
-            { navn: "Hækling",titel: "Strik og sjov hækling", forfatter: "Sophie Britten"},
-            { navn: "Krimi",titel:"Det forsvundne barn" , forfatter: "Linda Lassen"},
-            { navn: "Krimi",titel: "Havhesten", forfatter: "Vibeke Marx"}
-            { navn: "Ben Elton",titel: "I kamp mod tiden", forfatter: "Turbulenz"}
+            { navn: "Harry Potter",titel: "Flammernes pokal", forfatter: "J.K Rowiling",billede: "harry.webp"},
+            { navn: "Rolf og Ritter",titel: "Historier fra 20år", forfatter: "Dennis og Rolf",billede: "rolf.webp"},
+            { navn: "Hækling",titel: "Strik og sjov hækling", forfatter: "Sophie Britten",billede: "strik.webp"},
+            { navn: "Krimi",titel:"Det forsvundne barn" , forfatter: "Linda Lassen",billede: "det forsvunde spædbarn.webp"},
+            { navn: "Krimi",titel: "Havhesten", forfatter: "Vibeke Marx",billede: "havhesten.webp"},
+            { navn: "Ben Elton",titel: "I kamp mod tiden", forfatter: "Turbulenz",billede: "Ben elton.webp"}
         ];
 
-  
+
+        let output=document.getElementById("bogbox");
+
 
         for(i=0; i < book.length; i++){
-            document.getElementById()
+           let htmlBog = '<div class="bog"><img src="/img/ '+ book[i].billede +' "width="300px" height="400px" '+ book[i].titel + ' class="afstand"> <div class="bog__tekst"> <button onclick ="farvoritKnap('+ i +')">Farvorit</button> </div></div>'
+            output.innerHTML+=htmlBog;''
+    
+            console.log("teks2t");
         };
 
+        let farvoritliste=[ ];
+
+        function farvoritKnap(titel){
+             if(farvoritliste.includes(titel)){
+                alert("hello");
+            }
+            else(farvoritliste.includes(title))
+                
 
 
+            farvoritliste.push(titel);
 
-        function farvoritKnap(){
-            alert("hello")
-        }
+            console.log(farvoritliste);
 
+         
+          }
+        
+
+         
+
+       
+
+            
+        
